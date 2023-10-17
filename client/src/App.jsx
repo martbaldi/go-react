0659e6346a6b5b4fe9cbeeceb08c028f523499d1
martbaldi <baldinoiglesiasmartin@gmail.com>
2023-10-17 14:55:01 -0300

    build vite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,11 @@ function App() {
   return (
     <>
       <h1>Hello World!</h1>
-    </>
+      <button onClick={async ()=> {
+        const response =await fetch('http://localhost:3000/users')
+        const data= await response.json()
+        console.log(data)}}>Obtener datos</button>
+        </>
   )
 }
 
--- main.go
+++ main.go
@@ -5,13 +5,22 @@ import (
 	"fmt"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 
 	app := fiber.New()
+	app.Use(cors.New())
 	//Armo el ruteo, le digo que cuando ingrese a la ruta home, me redirija a la carpeta public.
-	app.Static("/", "./public")
+	app.Static("/", "./client/dist")
+	// le mando un objeto del tipo  contexto que tiene el resp y el request
+	// la funcion puerde retornar un error
+	app.Get("/users", func(c *fiber.Ctx) error {
+		return c.JSON(&fiber.Map{
+			"data": "usuarios desde el backend",
+		})
+	})
 	app.Listen(":3000")
 	fmt.Print("Server on PORT 3000")
 }
