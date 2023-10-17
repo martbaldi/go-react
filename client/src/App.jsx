b6d29977ae10bcd125858950e1642cbc00c4a186
martbaldi <baldinoiglesiasmartin@gmail.com>
2023-10-17 15:32:42 -0300

    Configuracion del proxy

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
       <h1>Hello World!</h1>
       <button onClick={async ()=> {
-        const response =await fetch('http://localhost:3000/users')
+        const response =await fetch('/users')
         const data= await response.json()
         console.log(data)}}>Obtener datos</button>
         </>
