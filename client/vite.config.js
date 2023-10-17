8aad8ca2e5b3970f10bfe6d19879980b3d00d3be
martbaldi <baldinoiglesiasmartin@gmail.com>
2023-10-17 16:00:18 -0300

    Correcion del proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/users': {
+        target: 'http://localhost:3000', // Reemplaza esto con la URL de tu servidor de backend
+        changeOrigin: true,
+       
+      },
+    },
+  },
 })
